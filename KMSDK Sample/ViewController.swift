cd52ae7e26b8c5bed68aace9c1750682ef549459
Rodrigo Pacheco Curro <rodrigo.pachecocurro@kiswe.com>
2017-03-09 17:25:30 -0500

    Adds ability to set event id before presenting media player controller

--- KMSDK Sample/ViewController.swift
+++ KMSDK Sample/ViewController.swift
@@ -11,26 +11,22 @@ import KMSDK
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var eventIdTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         KMSDK.shared.setAPIToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJLaXN3ZSIsInN1YiI6IjEzODU2MiIsImV4cCI6IjIwMTctMDktMDhUMjE6NTU6MzAuNzk0WiJ9.qgveH0QTXoHmJZRQNFeMl6Uy9kTtXXbKod-cj1x9bXI")
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        KMSDK.shared.mediaPlayerControllerWith(eventId: "5957") { (mediaVC: KMMediaViewController?) in
+    @IBAction func didTapPresentButton(_ sender: UIButton) {
+        guard let eventId = eventIdTextField.text else { return }
+        KMSDK.shared.mediaPlayerControllerWith(eventId: eventId) { (mediaVC: KMMediaViewController?) in
             if let mediaVC = mediaVC {
                 self.present(mediaVC, animated: true, completion: nil)
             }
         }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
